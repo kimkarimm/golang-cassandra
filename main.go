f4fa54fddc51c010e80c105e3822cf87fd01bf7f
karenmena <karenrosario.mena@bbva.com>
2018-01-02 14:54:51 -0600

    first commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	  "net/http"
+	    "log"
+	      "encoding/json"
+	        "github.com/gorilla/mux"
+		"github.com/golang-cassandra"
+	)
+
+type heartbeatResponse struct {
+	  Status string `json:"status"`
+	    Code int `json:"code"`
+    }
+
+func main() {
+	CassandraSession := Cassandra.Session
+	defer CassandraSession.Close()
+	  router := mux.NewRouter().StrictSlash(true)
+	    router.HandleFunc("/", heartbeat)
+	      log.Fatal(http.ListenAndServe(":8080", router))
+      }
+
+     func heartbeat(w http.ResponseWriter, r *http.Request) {
+	       json.NewEncoder(w).Encode(heartbeatResponse{Status: "OK", Code: 200})
+       }
+
